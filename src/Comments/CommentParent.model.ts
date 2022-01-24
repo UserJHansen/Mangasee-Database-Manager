5ca5e724869c574d39e8d4655c4f8c5c9e689b57
JHansen <47265397+UserJHansen@users.noreply.github.com>
2022-01-24 18:46:46 +1000

    Revert to standard format of classes

--- src/Comments/CommentParent.model.ts
+++
@@ -1,9 +0,0 @@
-import { HasMany, Model, Table } from 'sequelize-typescript';
-
-import Comment from './Comment.model';
-
-@Table
-export default class CommentParent<T> extends Model {
-  @HasMany(() => Comment)
-  comments: Comment<T>[];
-}
--- src/Replies/ReplyParent.model.ts
+++
@@ -1,9 +0,0 @@
-import { HasMany, Model, Table } from 'sequelize-typescript';
-
-import Reply from './Reply.model';
-
-@Table
-export default class ReplyParent<T> extends Model {
-  @HasMany(() => Reply)
-  replys: Reply<T>[];
-}
